6521e845c60180aca7817443adef777cf574c119
Ivan <i.dunskiy@gmail.com>
2014-01-13 20:22:21 +0200

    hi

--- js_client.js
+++ js_client.js
@@ -1,3 +1,43 @@
 /**
  * Created by RikiTikiTavi on 11.01.14.
  */
+
+function sendEmail(){
+    alert("im here")
+
+    var input_firstname = document.getElementById('tel_firstname').value;
+    var input_phone = document.getElementById('tel_phone').value;
+//    if (input_firstname & input_phone) {
+//        Recorder.record('tel_firstname', tel_firstname);
+//        Recorder.record('tel_phone', tel_phone);
+//        alert(input_firstname + input_phone);
+//    } else {
+//        alert('Please enter a filename!');
+//        input.focus();
+//    }
+
+//    alert(input_firstname + input_phone);
+    var link = "mailto:i.dunskiy@gmail.com"
+
+            + "&subject=" + escape(input_firstname)
+            + "&body=" + escape(input_phone)
+        ;
+
+    window.location.href = link;
+}
+$(function()
+{
+    $("#submit-button").click(function(){
+        $("#loading").fadeIn(100).show();
+        var from = $("#from").val();
+        var to = $("#to").val();
+        var subject = $("#subject").val();
+        var content = $("#content").val();
+        var data = "from=" + from + "&to=" + to + "&subject=" + subject + "&content=" + content;
+        if(to == ""){ $("#error-to").fadeIn(700).show(); $("#loading").fadeOut(100).hide();
+        }
+        else if(from == ""){
+            $("#error-from").fadeIn(700).show(); $("#loading").fadeOut(100).hide(); }else if(subject == "")
+        { $("#error-subject").fadeIn(700).show(); $("#loading").fadeOut(100).hide(); }else if(content == "")
+        { $("#error-content").fadeIn(700).show(); $("#loading").fadeOut(100).hide(); }else
+        { $.ajax({ type: "POST", url: "send.php", data: data, success: function(){ $("#loading").fadeOut(100).hide(); $('#message-sent').fadeIn(500).show(); } }); } }); });
---
+++ sending.php
@@ -0,0 +1,26 @@
+<html>
+<head><title>PHP Mail Sender</title></head>
+<body>
+<?php
+
+$message = '';
+
+if (isset($_POST['email']) && !empty($_POST['email'])){
+  if (mail($_POST['email'], $_POST['subject'], $_POST['body'], "From: name@yourdomain.com")){
+    $message = "Email has been sent to <b>".$_POST['email']."</b>.<br>";
+  }else{
+    $message = "Failed sending message to
+ <b>".$_POST['email']."</b>.<br>";
+  }
+}else{
+  if (isset($_POST['submit'])){
+$message = "No email address specified!<br>";
+  }
+}
+
+if (!empty($message)){
+   $message .= "<br><br>";
+ }
+?>
+</body>
+</html>
